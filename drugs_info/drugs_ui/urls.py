de160c56d546b0054f7a196db8b051a59bd402f4
Anatoly Zapravadov <zapokami@gmail.com>
2022-07-07 11:08:39 +0300

    implemented search similar drugs by drug and web ui for it

--- drugs_info/drugs_ui/urls.py
+++ drugs_info/drugs_ui/urls.py
@@ -4,5 +4,5 @@ from drugs_ui import views
 urlpatterns = [
     path('<int:pk>', views.index),
     path('add/', views.add_drug, name='add'),
-    path('search/<str:component>', views.drug_search)
+    path('search/<str:search_type>/<str:component>', views.drug_search_by_component),
 ]
