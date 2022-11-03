2526c05d8f667dfa196b1ab6924f58b41e8b6eb8
anza-afk <zapokami@gmail.com>
2022-11-03 13:47:07 +0300

    changing icontains to iregex for cyrillic symbols

--- drugs_info/drugs_info/urls.py
+++ drugs_info/drugs_info/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/drugs/', include('drugs_api.urls')),
+    path('api/v1/drugs', include('drugs_api.urls')),
     path('', include('drugs_ui.urls')),
 ]
